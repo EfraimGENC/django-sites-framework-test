fda832dc38fba121420317a2def410eb958f882e
Efraim GENÇ <efraim@kavim.digital>
2021-04-07 13:48:50 +0300

    apps folder created & moved

--- sitesf/settings.py
+++ sitesf/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'account',
-    'catalog',
+    'sitesf.apps.account',
+    'sitesf.apps.catalog',
 ]
 
 MIDDLEWARE = [
