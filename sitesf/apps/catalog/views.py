7f9af095fa107b09f92c3faf6718ae92aa232db6
EfraimGENC <efraim@kavimdigital.com>
2021-12-04 21:49:33 +0300

    cached views

--- sitesf/apps/catalog/views.py
+++ sitesf/apps/catalog/views.py
@@ -5,8 +5,13 @@ from .models import Product
 from django.contrib.sites.models import Site
 from sitesf.apps.channel.models import Channel
 from django.views.generic import ListView
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_cookie
+
 
 # Create your views here.
+@method_decorator([vary_on_cookie, cache_page(60)], name='dispatch')
 class ProductList(ListView):
     model = Product
     template_name='product_detail.html'
